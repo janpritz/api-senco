fdd3c7bfeafb6195b6156e054b577cdf2f7a79e0
janpritz <academics.johnfritzcabalhin@gmail.com>
2026-04-10 15:35:15 +0800

    update CORS configuration to allow requests from the production domain

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://senco.accsangkaychatbot.com/'],
 
     'allowed_origins_patterns' => [],
 
